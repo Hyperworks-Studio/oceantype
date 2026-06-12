3a8437280d28919d6eb933444476d57ea14a99b0
teakzc <tanitinpholsuwa@gmail.com>
2026-06-12 21:18:49 +0700

    testing fix for mobile scroll

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "./navbar";
 
 export default async function Current() {
     return (
-        <div className="h-screen overflow-hidden">
+        <div className="h-screen overflow-hidden overscroll-none">
             <Navbar />
             <Ocean />
             <LightRays />
